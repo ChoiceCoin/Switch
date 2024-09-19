cf12c35c0e2a5b35f40f3a04bbe2226d43c8bbe7
B Haney <43055154+Bhaney44@users.noreply.github.com>
2024-09-18 18:21:28 -0700

    Update switch.py

--- switch.py
+++ switch.py
@@ -1,18 +1,53 @@
+##############################
 # import everything from pyteal
 from pyteal import *
+##############################
 
+##############################
 # Function for generating approval program
 def approval_program():
+    ##############################
+
+    ##############################
     # Asset ID
     asset_id = Int(297995609)
-
     # Create app
     on_create = Approve()
-
     # Allow optin
     is_opt_in = Txn.on_completion() == OnComplete.OptIn
+    ##############################
 
-    # Check transaction
+    ############################################################
+    # Optin Logic
+    ############################################################
+    # Detect the funding transaction (1 Algo sent to the escrow)
+    ##############################
+    funding_amount = Int(1000000)  
+    is_funding_txn = And(
+        Txn.type_enum() == TxnType.Payment,                         
+        Txn.amount() >= funding_amount,                             
+        Txn.receiver() == Global.current_application_address()     
+    )
+    ##############################
+    # Logic for opt-in to the asset_id using an inner transaction
+    ##############################
+    escrow_opt_in = Seq([
+        InnerTxnBuilder.Begin(),
+        InnerTxnBuilder.SetFields({
+            TxnField.type_enum: TxnType.AssetTransfer,
+            TxnField.xfer_asset: asset_id,
+            TxnField.asset_receiver: Global.current_application_address(),  
+            TxnField.asset_amount: Int(0),  
+        }),
+        InnerTxnBuilder.Submit(),
+        Approve()  
+    ])
+    ##############################
+    
+    ############################################################
+    # App logic
+    ############################################################
+    # User specified amount
     user_specified_amount = Btoi(Txn.application_args[0])
     is_valid_transfer = And(
         Txn.type_enum() == TxnType.AssetTransfer,
@@ -20,25 +55,25 @@ def approval_program():
         Txn.asset_receiver() == Global.current_application_address(),
         Txn.asset_amount() == user_specified_amount 
     )
-
+    ##############################
     # Generate random number
+    ##############################
     round_number = Global.round()
     time_stamp = Global.latest_timestamp()
     combined = Sha256(Concat(Itob(round_number), Itob(time_stamp)))  
     normalized_random_value = Btoi(combined) % Int(2) 
-    
     ############################
     # return transaction to user
+    ############################
     send_to_user = InnerTxnBuilder.Execute({
         TxnField.type_enum: TxnType.AssetTransfer,
         TxnField.xfer_asset: asset_id,
-        TxnField.asset_amount: Mul(user_specified_amount, Int(2)),  
+        TxnField.asset_amount: Mul(user_specified_amount, Int(2)), 
         TxnField.asset_receiver: Txn.sender(),
     })
     ##############################
-
-    ##############################
     # return transaction to burn address
+    ##############################
     send_to_burn = InnerTxnBuilder.Execute({
         TxnField.type_enum: TxnType.AssetTransfer,
         TxnField.xfer_asset: asset_id,
@@ -46,38 +81,8 @@ def approval_program():
         TxnField.asset_receiver: Addr("6G5V4U2MCW5TIZ7JP6BZFQELTGGJBEG5EVSQRQQRLEZM3V6DXOPV5TUJQA"),
     })
     ##############################
-
-    ##############################
-    # Fund App account
-    app_fund = Seq([
-        InnerTxnBuilder.Begin(),
-        InnerTxnBuilder.SetFields({
-            TxnField.type_enum: TxnType.Payment,
-            TxnField.asset_receiver: Global.current_application_address(),
-            TxnField.asset_amount: Int(1000000)  
-        }),
-        InnerTxnBuilder.Submit(),
-        Approve()
-    ])
-    ##############################
-
-    ##############################
-    # Opt-in to CHOICE asset (297995609) for the application's account
-    app_asset_opt_in = Seq([
-        InnerTxnBuilder.Begin(),
-        InnerTxnBuilder.SetFields({
-            TxnField.type_enum: TxnType.AssetTransfer,
-            TxnField.xfer_asset: asset_id,
-            TxnField.asset_receiver: Global.current_application_address(),
-            TxnField.asset_amount: Int(0)  
-        }),
-        InnerTxnBuilder.Submit(),
-        Approve()
-    ])
-    ##############################
-
-    ##############################
     # conditional logic and execution
+    ##############################
     handle_random_txn = If(
         normalized_random_value == Int(1),
         Seq([send_to_user, Approve()]),  
@@ -86,12 +91,12 @@ def approval_program():
     ##############################
 
     ##############################
-    # Return
+    # Return Condition
+    ##############################
     return Cond(
-        [Txn.application_id() == Int(0), on_create],         
+        [Txn.application_id() == Int(0), on_create],           
         [is_opt_in, Approve()], 
-        [Txn.application_args[0] == Bytes("app_fund"), app_fund],                          
-        [Txn.application_args[0] == Bytes("optin_asset"), app_asset_opt_in],                                 
+        [is_funding_txn, escrow_opt_in],                             
         [is_valid_transfer, handle_random_txn],             
         [Int(1), Reject()]                                   
     )
